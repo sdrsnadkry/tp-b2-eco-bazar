10d32a0a99da07475f33250ff456cb15bb2fc972
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-07-22 08:43:28 +0545

    project tailwind config

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,41 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import localFont from 'next/font/local'
+
+
+// const inter = Inter({ subsets: ["latin"] });
+
+const poppins = localFont({
+  src: [
+    {
+      path: '../assets/fonts/Poppins-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Poppins-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Poppins-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Poppins-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Poppins-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+})
+
+
 
 export const metadata = {
   title: "Eco bazar",
@@ -11,7 +45,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className} >{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,9 @@
 export default function Home() {
   return (
     <main className="">
-      <h1 class="text-9xl	italic font-extrabold	text-orange-300	">Hello world!</h1>
 
-      <div className="">
-        <p className="text-blue-400 text-4xl">a</p>
-        <p>b</p>
+      <h1 className="text-h5 font-semibold" >Popular Categories</h1>
 
-        <button className="bg-success px-4 py-10">Click</button>
-      </div>
     </main>
   );
 }
