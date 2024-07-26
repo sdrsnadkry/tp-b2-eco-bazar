f25029a8fad1fedd61e1a28e6b24a7ad9bad5cff
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-07-26 08:58:57 +0545

    feat: added header

---
+++ src/app/_components/navLink.jsx
@@ -0,0 +1,16 @@
+import { dropdownIcon } from "@/assets/icons";
+import Image from "next/image";
+import React from "react";
+
+const NavLink = (props) => {
+  return (
+    <div className="flex items-center ml-8">
+      <span className={props.active ? "text-white" : "text-grey-500"}>{props.title}</span>
+      {!props.hideIcon && (
+        <Image src={dropdownIcon} alt="down-icon" className="w-5 h-5 ml-1" />
+      )}
+    </div>
+  );
+};
+
+export default NavLink;
--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,5 +2,20 @@ import dropdownIcon from "./dropdown.svg";
 import locationIcon from "./location.svg";
 import phoneIcon from "./phone.svg";
 import searchIcon from "./search.svg";
+import cartIcon from "./cart.svg";
+import dropDownIcon from "./downArrow.svg";
+import hamburgerIcon from "./hamburger.svg";
+import heartIcon from "./heart.svg";
+import userIcon from "./user.svg";
 
-export { dropdownIcon, locationIcon, phoneIcon, searchIcon };
+export {
+  dropdownIcon,
+  locationIcon,
+  phoneIcon,
+  searchIcon,
+  cartIcon,
+  dropDownIcon,
+  hamburgerIcon,
+  heartIcon,
+  userIcon,
+};
