3a9c10ba88d58cadd94f19d6353601220995b776
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-08-02 09:01:14 +0545

    async await

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,36 +1,66 @@
 "use client";
 
-import React from "react";
+import { useEffect } from "react";
+
+const promiseFunction = () => {
+  new Promise((resolve, reject) => {
+    const a = 1;
+    const b = 2;
+
+    const sum = a + b;
+
+    if (sum === 4) {
+      resolve("Success");
+    } else {
+      reject("Error");
+    }
+  })
+    .then((value) => {
+      console.log(value, "success");
+    })
+    .catch((error) => {
+      console.log(error, "error");
+    })
+    .finally(() => {
+      console.log("first finally");
+    });
+};
+
+const calculateSum = async () => {
+  const a = 1;
+  const b = 2;
+
+  const sum = a + b;
+
+  if (sum === 3) {
+    return "Success";
+  } else {
+    throw new Error("Error adding sum");
+  }
+};
 
 const Contact = () => {
-  const promiseFunction = () => {
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const a = 1;
-        const b = 2;
-
-        const sum = a + b;
-
-        if (sum === 3) {
-          resolve("Success");
-        } else {
-          reject("Error");
-        }
-      }, 2000);
-    })
-      .then((value) => {
-        console.log(value, "success");
-      })
-      .catch((error) => {
-        console.log(error, "error");
-      });
-
-      console.log(promise)
+  const getValue = async () => {
+    try {
+      const value = await calculateSum();
+      console.log(value);
+    } catch (error) {
+      console.log(error);
+    } finally{
+      console.log("FInally")
+    }
   };
 
-  promiseFunction();
+  useEffect(() => {
+    getValue();
+  }, []);
 
-  return <div>Contact</div>;
+  return (
+    <div>
+      Contact
+      <button onClick={promiseFunction}>CLick</button>
+    </div>
+  );
 };
 
 export default Contact;
